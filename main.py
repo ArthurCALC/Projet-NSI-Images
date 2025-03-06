e4c3cf403ed299feb44fac32c093813ddb6b37a5
Arthur Cantat <cantat.i.arthur@gmail.com>
2025-03-06 21:37:31 +0100

    J'ai ajoute mes fonctions je met solal apres

--- main.py
+++ main.py
@@ -1,37 +1,64 @@
-def option_one():
-    print("You selected Option 1.")
+from PIL import Image, ImageEnhance
 
-def option_two():
-    print("You selected Option 2.")
+def noir_et_blanc(chemin_image, seuil):
+    image = Image.open(chemin_image).convert("RGB")
+    largeur, hauteur = image.size
+    for x in range(largeur):
+        for y in range(hauteur):
+            r, g, b = image.getpixel((x, y))
+            moyenne = (r + g + b) // 3
+            couleur = 255 if moyenne > seuil else 0
+            image.putpixel((x, y), (couleur, couleur, couleur))
+    return image
 
-def option_three():
-    print("You selected Option 3.")
+def effet_jeu_video(chemin_image, n):
+    image = Image.open(chemin_image).convert("RGB")
+    pixels = image.load()
+    step = 256 // n
+    for x in range(image.width):
+        for y in range(image.height):
+            r, g, b = pixels[x, y]
+            pixels[x, y] = (r // step * step, g // step * step, b // step * step)
 
-def exit_program():
-    print("Exiting program.")
-    exit()
+    return image
 
-def main_menu():
-    while True:
-        print("\nMenu:")
-        print("1. Run Option 1")
-        print("2. Run Option 2")
-        print("3. Run Option 3")
-        print("4. Exit")
+def apply_vintage_effect(image):
 
-        choice = input("Enter your choice: ")
+    image = Image.open(chemin_image)
+    image = image.convert("RGB")
 
-        if choice == "1":
-            option_one()
-        elif choice == "2":
-            option_two()
-        elif choice == "3":
-            option_three()
-        elif choice == "4":
-            exit_program()
-        else:
-            print("Invalid choice. Please enter a number between 1 and 4.")
+    couleur = ImageEnhance.Color(image)
+    image = couleur.enhance(0.5)
+    lumiere = ImageEnhance.Brightness(image)
+    image = lumiere.enhance(0.9)
+    return image
 
+i=0
 if __name__ == "__main__":
-    main_menu()
+     while True:
+        print("Choisissez un effet :")
+        print("1 - Noir et blanc")
+        print("2 - Effet jeu vidéo")
+        print("3 - Effet Vintage")
+        print("4 - Sortir")
+        choix = input("Entrez le chiffre : ")
 
+        if choix == "4":
+            break
+        chemin_image = input("Entrez le chemin de l'image : ")
+        if choix == "1":
+            seuil = int(input("Entrez le seuil (0-255) : "))
+            image_modifiee = noir_et_blanc(chemin_image, seuil)
+        elif choix == "2":
+            n = int(input("Entrez le nombre de niveaux (ex: 8) : "))
+            image_modifiee = effet_jeu_video(chemin_image, n)
+        elif choix == "3":
+            image_modifiee = apply_vintage_effect(chemin_image)
+        else:
+            print("Choix invalide.")
+        image_modifiee.show()
+        if "image_modifiee" +str(i) +".jpg":
+            i += 1
+            image_modifiee.save("image_modifiee" + str(i) + ".jpg")
+        else:
+            image_modifiee.save("image_modifiee.jpg")
